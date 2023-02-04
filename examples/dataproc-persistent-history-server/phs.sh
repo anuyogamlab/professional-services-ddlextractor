42ed6fe4c4974e2ac0e31d3d90a00027e5e87deb
anuyogamlab <61569349+anuyogamlab@users.noreply.github.com>
2023-02-04 08:39:10 -0500

    Create phs.sh

---
+++ examples/dataproc-persistent-history-server/phs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Create a Persistent History Server (PHS) on Dataproc 2.x
+# To setup Persistent History Server (PHS) on Dataproc 1.5, modify --image-version 
+
+# Setup environment variables
+export PHS_CLUSTER_NAME=<CLUSTER NAME>
+export REGION=<REGION>
+export ZONE=<ZONE>
+export GCS_BUCKET=<GCS BUCKET>
+export PROJECT_NAME=<PROJECT NAME>
+
+# Create PHS pointing to the GCS_BUCKET
+gcloud dataproc clusters create $PHS_CLUSTER_NAME \
+--enable-component-gateway \
+--region ${REGION} --zone $ZONE \
+--single-node \
+--master-machine-type n2-highmem-4 \
+--master-boot-disk-size 500 \
+--image-version 2.0-debian10 \
+--properties \
+yarn:yarn.nodemanager.remote-app-log-dir=gs://$GCS_BUCKET/yarn-logs,\
+mapred:mapreduce.jobhistory.done-dir=gs://$GCS_BUCKET/events/mapreduce-job-history/done,\
+mapred:mapreduce.jobhistory.intermediate-done-dir=gs://$GCS_BUCKET/events/mapreduce-job-history/intermediate-done,\
+spark:spark.eventLog.dir=gs://$GCS_BUCKET/events/spark-job-history,\
+spark:spark.history.fs.logDirectory=gs://$GCS_BUCKET/events/spark-job-history,\
+spark:SPARK_DAEMON_MEMORY=16000m,\
+spark:spark.history.custom.executor.log.url.applyIncompleteApplication=false,\
+spark:spark.history.custom.executor.log.url={{YARN_LOG_SERVER_URL}}/{{NM_HOST}}:{{NM_PORT}}/{{CONTAINER_ID}}/{{CONTAINER_ID}}/{{USER}}/{{FILE_NAME}} \
+--project $PROJECT_NAME
